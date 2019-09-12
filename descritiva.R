071e98006fb504c8820141f81ea7beb00246fa94
bbiasi <brennerbiasiss@gmail.com>
2019-09-12 17:39:13 -0300

    descritiva

---
+++ descritiva.R
@@ -0,0 +1,15 @@
+setwd("D:/PEI/Doc/Disciplinas/Topicos/topicos_eng436")
+
+library(dplyr)
+library(readxl)
+
+# Carregando ds e realizando manipulacoes basicas ----
+dataset <- readxl::read_excel("glass.xlsx")
+dplyr::glimpse(dataset)
+
+df <- dataset %>% 
+  dplyr::mutate_if(is.character, as.numeric) %>% 
+  dplyr::mutate(Tipo = as.factor(Type)) %>% 
+  dplyr::select(-Type)
+
+dplyr::glimpse(df)
