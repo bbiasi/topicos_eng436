f641709462ab43b17a1d9c03a9d37f457055f969
bbiasi <brennerbiasiss@gmail.com>
2019-09-12 18:16:10 -0300

    script

--- descritiva.R
+++ descritiva.R
@@ -12,4 +12,33 @@ df <- dataset %>%
   dplyr::mutate(Tipo = as.factor(Type)) %>% 
   dplyr::select(-Type)
 
+levels(df$Tipo) <- make.names(levels(factor(df$Tipo)))
+
 dplyr::glimpse(df)
+table(df$Tipo)
+
+
+
+{
+  set.seed(1)
+  index <- caret::createDataPartition(df$Tipo, p = 0.75, list = FALSE) 
+  train <- df[index, ]
+  test  <- df[-index, ]
+  
+  ctrl  <- caret::trainControl(method  = "cv", 
+                               number  = 10,
+                               classProbs = T,
+                               savePredictions = T,
+                               verboseIter = FALSE,
+                               allowParallel = TRUE)
+  
+  model1_r_rf <- caret::train(y = train$Tipo,
+                              x = train[, "Tipo"],
+                              method = "rf",
+                              preProcess = c("scale", "center"),
+                              metric     = "Accuracy",
+                              trControl  = ctrl,
+                              ntree = 1500)
+}
+
+model1_r_rf
