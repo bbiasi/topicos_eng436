1646b663e59719db295aa81188a108065c6b90c0
bbiasi <brennerbiasiss@gmail.com>
2019-09-25 15:39:10 -0300

    aa

--- descritiva.R
+++ descritiva.R
@@ -18,28 +18,3 @@ dplyr::glimpse(df)
 table(df$Tipo)
 
 
-# aaaaa
-### aaaa
-{
-  set.seed(1)
-  index <- caret::createDataPartition(df$Tipo, p = 0.75, list = FALSE) 
-  train <- df[index, ]
-  test  <- df[-index, ]
-  
-  ctrl  <- caret::trainControl(method  = "cv", 
-                               number  = 10,
-                               classProbs = T,
-                               savePredictions = T,
-                               verboseIter = FALSE,
-                               summaryFunction = specmine::multiiClassSummary)
-  
-  model1_r_rf <- caret::train(y = train$Tipo,
-                              x = train[, "Tipo"],
-                              method = "rf",
-                              preProcess = c("scale", "center"),
-                              metric     = "Accuracy",
-                              trControl  = ctrl,
-                              ntree = 1500)
-}
-
-model1_r_rf
