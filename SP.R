3b50dca81062d5b8602444f6c1dad45c30c276f7
marcellopessoa <marcellllo-10@hotmail.com>
2019-10-31 22:09:47 -0300

    SP att

--- SP.R
+++ SP.R
@@ -2,7 +2,7 @@ setwd("C:/Users/MARCELLO/Desktop/ENG436/topicos_eng436")
 
 #Análise supervisionada
 
-##Manipulando o banco de dados----
+##Bibliotecas----
 
 library(readxl)
 library(dplyr)
@@ -18,6 +18,8 @@ library(caret)
 library(e1071)
 library(randomForest)
 
+##Manipulando o banco de dados----
+
 dataset <- readxl::read_excel("glass.xlsx")
 dplyr::glimpse(dataset)
 
@@ -31,22 +33,17 @@ levels(df$Tipo) <- make.names(levels(factor(df$Tipo)))
 dplyr::glimpse(df)
 table(df$Tipo)
 
-#Teste-Treino----
-dfx2 <- dfx2 %>% 
-  select(-rowid,-Out)
-  
-dfx2 <- dfx2 %>% 
-  mutate(CLASSE = as.factor(CLASSE))
+##Teste-Treino----
 
 set.seed(42)
-index <- createDataPartition(dfx2$CLASSE, p = 0.7, list = FALSE)
-train_data <- dfx2[index, ]
-test_data  <- dfx2[-index, ]
+index <- createDataPartition(df$Tipo, p = 0.7, list = FALSE)
+train_data <- df[index, ]
+test_data  <- df[-index, ]
 
 ##RF----
 
 set.seed(42)
-model_rf <- caret::train(CLASSE ~ .,
+model_rf <- caret::train(Tipo ~ .,
                          data = train_data,
                          method = "rf",
                          preProcess = c("scale", "center"),
@@ -58,7 +55,7 @@ model_rf <- caret::train(CLASSE ~ .,
                                                   
                          
 predictions <- predict(model_rf,test_data)
-cm_original <- confusionMatrix(predictions, test_data$CLASSE)
+cm_original <- confusionMatrix(predictions, test_data$Tipo)
   
 ##KNN----
 
@@ -135,34 +132,6 @@ df_rf_original_up <- data.frame(Acuracia = cm_original_up$overall[1],
                              Especi = cm_original_up$byClass[2],
                              F1 = cm_original_up$byClass[7])
 
-
-
-###GrÁFICOS
 models <- df_rf_original %>% 
-  dplyr::bind_rows(df_rf_original_up, df_rf_original_down) %>% 
-  reshape2::melt(id.vars = c("resamp")) %>% 
-  dplyr::rename("Metrica" = variable,
-                "Valor" = value) %>% 
-  dplyr::mutate(resamp  = as.factor(resamp))
+  dplyr::bind_rows(df_rf_original_up, df_rf_original_down)
 
-levels(models$resamp)  <- c("Original", "Over", "Under")
-levels(models$Metrica) <- c("Acurácia", "Kappa", "Sensibilidade", 
-                            "Especificidade", "F1")
-
-pmodelo <- ggplot2::ggplot(models) +
-  geom_point(aes(x = Metrica,
-                 y = Valor,
-                 fill = resamp),
-             shape = 21, alpha = 0.7, stroke = 1.5,
-             size = 5) +
-  facet_wrap(~Cenario, nrow = 1) +
-  scale_fill_manual(name = "Legenda",
-                    values = wesanderson::wes_palette("Darjeeling1")) +
-  #scale_y_continuous(limits = c(0, 1)) +
-  guides(fill = guide_legend(override.aes = list(size = 4))) +
-  xlab("Métrica") + ylab("Valor") +
-  temag + theme(axis.text.x  = element_text(angle = 90),
-                legend.position = "bottom",
-                strip.text = element_text(size = 20, face = "bold"))
-p_save_modeloc <- pmodelo
-pmodelo
